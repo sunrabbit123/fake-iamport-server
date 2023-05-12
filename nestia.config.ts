8551d7a29e394b7d89085f20fc789bb31f2e3278
Jeongho Nam <samchon.github@gmail.com>
2023-05-12 19:00:30 +0900

    Enhance swagger config

--- nestia.config.ts
+++ nestia.config.ts
@@ -5,6 +5,21 @@ const NESTIA_CONFIG: INestiaConfig = {
     output: "src/api",
     swagger: {
         output: "dist/swagger.json",
+        info: {
+            title: "Iamport API",
+            description:
+                "Built by [fake-iamport-server](https://github.com/samchon/fake-iamport-server) with [nestia](https://github.com/samchon/nestia)",
+        },
+        servers: [
+            {
+                url: "http://localhost:10851",
+                description: "fake",
+            },
+            {
+                url: "https://api.iamport.kr",
+                description: "real",
+            },
+        ],
         security: {
             bearer: {
                 type: "apiKey",
